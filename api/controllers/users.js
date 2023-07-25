4af2b765455136522505d201d238a0e3311aadd7
Fene-87 <fenekayas87@gmail.com>
2023-07-26 00:43:49 +0300

    Create endpoint to update user and save changes to DB

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -9,4 +9,31 @@ export const createUser = async (req, res, next) => {
     } catch (error) {
         return res.status(500).send("Internal Server Error");
     }
-}
+};
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({});
+        return res.status(200).send(users);
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
+    }
+};
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if(user) {
+            const newData = await User.findByIdAndUpdate(
+                req.params.id,
+                {$set: req.body},
+                {new: true}
+            )
+            return res.status(200).send("User successfully updated");
+        } else {
+            return res.status(404).send("User not found");
+        }
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
+    }
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { createUser } from "../controllers/users.js";
+import { createUser, getUsers, updateUser } from "../controllers/users.js";
 
 const router = express.Router();
 
+// Create new user
 router.post('/create', createUser);
+
+// Fetch all users
+router.get('/', getUsers);
+
+// Update user
+router.put('/update/:id', updateUser);
 
 export default router;
